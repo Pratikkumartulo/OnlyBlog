2dbc61d9e91371cc37a59e4b9b3c3abb4b6b7b91
pratikkumartulo <pratiktulo42@gmail.com>
2024-11-03 06:38:36 +0530

    third commit

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ const conf = {
     appwriteDatabaseID : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionID : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketID : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    rteapikey : String(import.meta.env.VITE_RTE_API)
+    rteapikey : String(import.meta.env.VITE_APPWRITE_RTE_API)
 }
 
 export default conf
